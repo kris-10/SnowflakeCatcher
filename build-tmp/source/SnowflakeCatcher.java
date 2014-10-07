1f81d5f20e8240c571f59c9458e74d81991bcf5d
unknown <krleung@697-334-Shelob.sf.k12.internal>
2014-10-07 11:27:21 -0700

    awrar

--- build-tmp/source/SnowflakeCatcher.java
+++ build-tmp/source/SnowflakeCatcher.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SnowflakeCatcher extends PApplet {
+
 SnowFlake[]frosty = new SnowFlake[1000];
-void setup()
+public void setup()
 {
   size (400,400);
   background(0);
@@ -9,7 +25,7 @@ void setup()
   }
   //your code here
 }
-void draw()
+public void draw()
 {
   for (int i=0; i<frosty.length; i++)
   {
@@ -24,7 +40,7 @@ void draw()
 
   //your code here
 }
-void mouseDragged()
+public void mouseDragged()
 {
   fill(254);
   noStroke();
@@ -45,14 +61,14 @@ class SnowFlake
     isMoving=false;
     //class member variable initializations
   }
-  void show()
+  public void show()
   {
     noStroke();
     fill(255);
     ellipse(myX,myY,ranSiz,ranSiz);
     //your code here
   }
-  void lookDown()
+  public void lookDown()
   {
     if (myY<400 && myY>0 && (get(myX, myY+7) != color(255)) && (get (myX, myY+8) != color (0)))
     {
@@ -64,13 +80,13 @@ class SnowFlake
     }
     //your code here
   }
-  void erase()
+  public void erase()
   {
     fill(0);
-    ellipse(myX,myY-1,ranSiz+6,ranSiz+6); 
+    ellipse(myX,myY-1,ranSiz+4,ranSiz+4); 
     //your code here
   }
-  void move()
+  public void move()
   {
     if (isMoving)
     //your code here
@@ -78,7 +94,7 @@ class SnowFlake
     myY++;
   }
   }
-  void wrap()
+  public void wrap()
   {
     if (myY>390)
     {
@@ -90,3 +106,12 @@ class SnowFlake
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SnowflakeCatcher" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
